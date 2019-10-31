4e1f7e8075008cf2f8ca118dd25ff569331a36ab
Magneig <57160044+Magneig@users.noreply.github.com>
2019-10-30 19:51:22 -0500

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,9 @@
+price = 10000
+cheap_car = False
+expensive_car= True
+if cheap_car:
+    cheap = price * 1
+    print(f"This Chevrolet will cost you: ${cheap}!")
+else:
+    expensive = price * 10
+    print(f"This Lamborghini will cost you: ${expensive}!")
